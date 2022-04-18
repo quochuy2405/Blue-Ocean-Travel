f9665ad96efc7ef987c0c7132aea11819e5616f9
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-18 16:00:50 +0700

    first commit

--- src/Components/Travel.tsx
+++ src/Components/Travel.tsx
@@ -1,5 +1,4 @@
 import { IconCalendarClock, IconIdCard } from '@douyinfe/semi-icons'
-import { Tooltip } from '@douyinfe/semi-ui'
 import React from 'react'
 import { ITravel } from '../Interface'
 import '../Styles/Travel.scss'
@@ -10,17 +9,14 @@ const Travel = (travel: ITravel) => {
         <img src={travel.thumbnail} alt="anh" />
       </div>
       <div className="info-travel">
-        <Tooltip content={travel.date}>
-          <div className="icon-date">
-            <IconCalendarClock size="large" /> <span>{travel.date.slice(0, 5)}</span>
-          </div>
-        </Tooltip>
-        <Tooltip content={travel.tour_guide}>
-          <div className="icon-tour-guide">
-            <IconIdCard size={'large'} />{' '}
-            <span>{travel.tour_guide.split(' ')[travel.tour_guide.split(' ').length - 1]}</span>
-          </div>
-        </Tooltip>
+        <div className="icon-date">
+          <IconCalendarClock size="large" /> <span>{travel.date.slice(0, 5)}</span>
+        </div>
+
+        <div className="icon-tour-guide">
+          <IconIdCard size={'large'} />{' '}
+          <span>{travel.tour_guide.split(' ')[travel.tour_guide.split(' ').length - 1]}</span>
+        </div>
       </div>
       <div className="description-travel">
         <p>
