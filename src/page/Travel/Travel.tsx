876f04d2b75c87cdb6667437a5b9a9a69f086b15
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-19 08:08:22 +0700

    structure

--- src/page/Travel/Travel.tsx
+++ src/page/Travel/Travel.tsx
@@ -1,12 +1,14 @@
-import { Header } from "@/components";
+import { Footer, Header, Slider } from "@/components";
 import  styles  from "./Travel.module.scss"
 const Travel = ():JSX.Element => {
     return (
         <div>
             <Header />
              <div className={styles.bodyPage}>
-
+                <Slider />
+                
             </div>
+            <Footer/>
         </div>
     );
 };
