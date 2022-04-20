e033de7005222b5dd16efd6ed6383bbacda7df4f
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-20 22:31:30 +0700

    done

--- src/components/TourGuide/TourGuide.tsx
+++ src/components/TourGuide/TourGuide.tsx
@@ -1,34 +1,13 @@
 import type { ITourGuide } from '@/types'
 import styles from './TourGuide.module.scss'
 
-const TourGuide = (tour: ITourGuide) => {
+const TourGuide = ({br }:{ br: any }) => {
   return (
     <div className={styles.tourGuide}>
       <div className={styles.br}>
-        <img src={tour.br} alt="background" />
+        <img src={br} alt="background" />
       </div>
-      <div className={styles.tourDetails}>
-        <div className={styles.avatar}>
-          <img src={tour?.avatar} alt="avatar" />
-        </div>
-        <div className={styles.content}>
-          <div className="name">
-            <p>NAME: {tour.name}</p>
-          </div>
-          <div className="dob">
-            <p>D.O.B: {tour.DOB}</p>
-          </div>
-          <div className="position">
-            <p>POSITION: {tour.position}</p>
-          </div>
-          <div className="code">
-            <p>CODE: {tour.code}</p>
-          </div>
-          <div className="tel">
-            <p>TEL: {tour.tel}</p>
-          </div>
-        </div>
-      </div>
+  
     </div>
   )
 }
--- src/components/TourGuideList/TourGuideList.tsx
+++ src/components/TourGuideList/TourGuideList.tsx
@@ -1,100 +1,25 @@
 import TourGuide from '../TourGuide'
-import avatar1 from '/images/place1.png'
-import br1 from '/images/br1.png'
-import br2 from '/images/br2.png'
-import br3 from '/images/br3.png'
-import br4 from '/images/br4.png'
+import tourguide1 from '@/../public/images/tourguide/tourguide1.png'
+import tourguide2 from '@/../public/images/tourguide/tourguide2.png'
+import tourguide3 from '@/../public/images/tourguide/tourguide3.png'
+import tourguide4 from '@/../public/images/tourguide/tourguide4.png'
 import styles from './TourGuideList.module.scss'
 import type { ITourGuide } from '@/types'
 
-const ListTourGuide: ITourGuide[] = [
-  {
-    avatar: avatar1,
-    name: 'Trần Thị Bích Cẩm',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br3,
-  },
-   {
-    avatar: avatar1,
-    name: 'Nguyễn Linh Ngân',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br2,
-  },
-    {
-    avatar: avatar1,
-    name: 'Bùi Thị Như Quỳnh',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br3,
-  },
-     {
-    avatar: avatar1,
-    name: 'Nhu Hang Le',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br4,
-  },
-      {
-    avatar: avatar1,
-    name: 'Thu Hà',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br3,
-  },
-  {
-    avatar: avatar1,
-    name: 'Trần Hoàn Vi',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br2,
-  },
-  {
-    avatar: avatar1,
-    name: 'Tuyền Phạm',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br4,
-  },
-  {
-    avatar: avatar1,
-    name: 'Da Eun',
-    DOB: '07/04/2000',
-    position: 'Tour Guide',
-    code: '1212',
-    tel: '029122912',
-    br: br3,
-  },
-]
-
+const ListTourGuide = [tourguide1,tourguide2,tourguide3,tourguide4]
 const TourGuideList = () => {
   return (
     <>  <div className={styles.enjoy}>
-              <p>Tour Guide </p>
-            </div>
-            <div className={styles.title}>
-              <p>CONTACT</p>
-            </div>
-    <div className={styles.tourGuideList}>
-      {ListTourGuide.map((item: ITourGuide, index) => (
-        <TourGuide {...item} key={index} />
-      ))}
+      <p>Tour Guide </p>
     </div>
+      <div className={styles.title}>
+        <p>CONTACT</p>
+      </div>
+      <div className={styles.tourGuideList}>
+        {ListTourGuide.map((item, index) => (
+          <TourGuide br={item} key={index} />
+        ))}
+      </div>
     </>
   )
 }
