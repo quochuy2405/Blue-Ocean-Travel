e2bcaa90c3270876f59078bcaf4bb6b259958167
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-04-20 22:35:31 +0700

    done

--- src/components/TourGuideList/TourGuideList.tsx
+++ src/components/TourGuideList/TourGuideList.tsx
@@ -3,10 +3,11 @@ import tourguide1 from '@/../public/images/tourguide/tourguide1.png'
 import tourguide2 from '@/../public/images/tourguide/tourguide2.png'
 import tourguide3 from '@/../public/images/tourguide/tourguide3.png'
 import tourguide4 from '@/../public/images/tourguide/tourguide4.png'
+import tourguide5 from '@/../public/images/tourguide/tourguide5.png'
 import styles from './TourGuideList.module.scss'
 import type { ITourGuide } from '@/types'
 
-const ListTourGuide = [tourguide1,tourguide2,tourguide3,tourguide4]
+const ListTourGuide = [tourguide1,tourguide2,tourguide3,tourguide4,tourguide5]
 const TourGuideList = () => {
   return (
     <>  <div className={styles.enjoy}>
